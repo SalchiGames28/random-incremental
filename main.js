29bf22c002177c6e228e11b9b4a1073298abe6df
l1mb0s <96627718+l1mb0s@users.noreply.github.com>
2023-05-29 20:05:11 -0300

    added version

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var game = {
     knowledgePoints: 0,
     totalKP: 0,
     tempMultiplierIncrease: 0.3,
+    version: '0.0.10',
     
     addMoney: function(amount) {
         this.money = Decimal.plus(this.money.toString(), amount.toString()).toString();
